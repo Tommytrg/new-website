dfc03e4d203a165ba42072df6508cb979c9e61a2
tommytrg <tomastrg22@gmail.com>
2021-02-23 11:18:59 +0100

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -65,26 +65,28 @@
     </div>
 
     <div class="decentralized-oracle">
-      <div class="content">
-        <div class="text">
-          <h2 class="title">
-            The decentralized oracle that is true to the censorship resistant
-            nature of blockchains.
-          </h2>
-          <p class="paragraph">
-            Without a reliable oracle, smart contracts can be vulnerable to
-            hacks, corruption and monetary loss.
-          </p>
-          <p class="paragraph">
-            Witnet leverages state-of-the-art cryptographic and economic
-            techniques to provide your smart contracts with secure datainput.
-          </p>
+      <div class="wrapper">
+        <div class="content">
+          <div class="text">
+            <h2 class="title">
+              The decentralized oracle that is true to the censorship resistant
+              nature of blockchains.
+            </h2>
+            <p class="paragraph">
+              Without a reliable oracle, smart contracts can be vulnerable to
+              hacks, corruption and monetary loss.
+            </p>
+            <p class="paragraph">
+              Witnet leverages state-of-the-art cryptographic and economic
+              techniques to provide your smart contracts with secure datainput.
+            </p>
+          </div>
+          <div class="image">
+            <p class="illustration">(Nice illustration here)</p>
+          </div>
         </div>
-        <div class="image">
-          <p class="illustration">(Nice illustration here)</p>
-        </div>
+        <button class="button black margin-top">> Build with witnet</button>
       </div>
-      <button class="button black">> Build with witnet</button>
     </div>
 
     <div>testimonials</div>
@@ -263,44 +265,53 @@ export default {}
     border-radius: 6px;
   }
 }
+
 .decentralized-oracle {
-  .content {
-    background: red;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    align-items: center;
-    justify-items: center;
+  display: flex;
+  justify-content: center;
 
-    .text {
-      max-width: 554px;
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 1280px;
 
-      .title {
-        font-size: 40px;
-        font-weight: bold;
-        margin-bottom: 32px;
+    .content {
+      width: 100%;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+
+      .text {
+        max-width: 554px;
+
+        .title {
+          font-size: 40px;
+          font-weight: bold;
+          margin-bottom: 32px;
+        }
+
+        .paragraph {
+          font-size: 18px;
+          margin-bottom: 16px;
+        }
       }
-
-      .paragraph {
-        font-size: 18px;
-        margin-bottom: 16px;
+      .image {
+        justify-self: center;
+        .illustration {
+          border-radius: 50%;
+          background: #fff;
+          width: 300px;
+          height: 300px;
+          align-items: center;
+          display: flex;
+          justify-content: center;
+        }
       }
     }
-    .image {
-      .illustration {
-        border-radius: 50%;
-        background: #fff;
-        width: 300px;
-        height: 300px;
-        align-items: center;
-        display: flex;
-        justify-content: center;
-      }
-    }
-  }
 
-  .btn {
-    color: black;
-    border: black;
+    .margin-top {
+      margin-top: 16px;
+    }
   }
 }
 .announcement {
